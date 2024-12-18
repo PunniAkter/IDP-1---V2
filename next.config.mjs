a727ddcf0c59e56f276567d5bac9c7ef1f7a9160
PunniAkter <punniakter094@gmail.com>
2024-12-18 06:53:02 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-    eslint: {
-=======
   eslint: {
->>>>>>> 75234ee0b74ec9655506070e9f87c972780f7945
-        ignoreDuringBuilds: true,
-    },
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
